c1fcc56ec3e8908b7f4a62280196f1ec0ded7022
TmsRC <t.rubio28@uniandes.edu.co>
2019-03-28 12:30:31 +0000

    Primer intento

---
+++ complejo.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cmath
+
+class Complejo:
+    def __init__(self,a,b):
+        self.imaginario = b
+        self.real = a
+        self.norma = (a**2 + b**2)**(1/2)
+    def confugado(self):
+        return Complejo(self.real,self.imaginario)
+    def calcula_norma(self):
+        return self.norma
+    def pow(self,p):
+        z = np.power((self.real + 1j*self.imaginario),p).real
+        return Complejo(z.real,z.imag)
